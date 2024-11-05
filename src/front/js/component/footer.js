ad54d96190841bba09eb6b88da4bc76a3ba9251c
Sharguidev <guillermoj.obando@gmail.com>
2024-11-05 16:47:55 -0600

    FrontEnd de Login Listo

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -7,21 +7,29 @@ export function Footer() {
 		<div className="container-footer mt-5">
 			<footer className="pie-pagina">
 				<div className="grupo-1">
-					<div className="box">
-						<figure className="imagen">
-							<a href="#">
-								<img alt="imagen" src="https://github.com/4GeeksAcademy/Authentication-System-Sharguidev/blob/main/src/front/img/Logo_principal.png?raw=true" />
-							</a>
-						</figure>
+					<div className="row">
+						<div className="col-md-8">
+							<div className="box">
+								<figure className="imagen">
+									<a href="#">
+										<img alt="imagen" src="https://github.com/4GeeksAcademy/Authentication-System-Sharguidev/blob/main/src/front/img/Sharguidev_s.png?raw=true" />
+									</a>
+								</figure>
+							</div>
+
+						</div>
+
 					</div>
 					<div className="box">
-						<h2>Sobre Nosotros</h2>
+						<h2 className="Aus">About Us</h2>
 						<p>
-							Roomies es una plataforma que permite a los usuarios publicar propiedades en la
-							red. Los usuarios podrán ver propiedades publicadas por otros usuarios y contactar a los propietarios.</p>
+							We are a team of developers who are passionate about creating innovative solutions for our clients. Making their businesses and lives easier.
+							Doing <br />
+							great work is what we do best.
+						</p>
 					</div>
 					<div className="box">
-						<h2>Siguenos</h2>
+						<h2 className="Fus">Follow Us</h2>
 						<div className="red-social">
 							<a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
 								<i className="fa-brands fa-facebook"></i>
@@ -37,7 +45,7 @@ export function Footer() {
 				</div>
 
 				<div className="grupo-2">
-					<small>&copy; 2024 <b>Roomies</b> - Todos los derechos reservados.</small>
+					<small>&copy; 2024 <b>Sharguidev</b> - Todos los derechos reservados.</small>
 				</div>
 			</footer>
 		</div>
