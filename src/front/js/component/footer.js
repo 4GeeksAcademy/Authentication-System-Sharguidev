4b95d8a429dd175a928968a0dbad7d3b26074c6a
Sharguidev <guillermoj.obando@gmail.com>
2024-11-05 15:36:46 -0600

    importacion de logos y cambios en footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./../../styles/footer.css";
 
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+export function Footer() {
+	return (
+		<div className="container-footer mt-5">
+			<footer className="pie-pagina">
+				<div className="grupo-1">
+					<div className="box">
+						<figure className="imagen">
+							<a href="#">
+								<img alt="imagen" src="https://github.com/4GeeksAcademy/Authentication-System-Sharguidev/blob/main/src/front/img/Logo_principal.png?raw=true" />
+							</a>
+						</figure>
+					</div>
+					<div className="box">
+						<h2>Sobre Nosotros</h2>
+						<p>
+							Roomies es una plataforma que permite a los usuarios publicar propiedades en la
+							red. Los usuarios podrán ver propiedades publicadas por otros usuarios y contactar a los propietarios.</p>
+					</div>
+					<div className="box">
+						<h2>Siguenos</h2>
+						<div className="red-social">
+							<a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+								<i className="fa-brands fa-facebook"></i>
+							</a>
+							<a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+								<i className="fa-brands fa-instagram"></i>
+							</a>
+							<a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+								<i className="fa-brands fa-twitter"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div className="grupo-2">
+					<small>&copy; 2024 <b>Roomies</b> - Todos los derechos reservados.</small>
+				</div>
+			</footer>
+		</div>
+	);
+}
