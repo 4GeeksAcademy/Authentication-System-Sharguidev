99243af2eabbbb5e730989d00d17e902f7001851
Sharguidev <guillermoj.obando@gmail.com>
2024-11-06 10:33:13 -0600

    Cambios en el Navbar y logo principla reduccion de tamaño

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+			<div className="container-fluid py-1">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0">
+						<img alt="imagen" src="https://github.com/4GeeksAcademy/Authentication-System-Sharguidev/blob/main/src/front/img/Sharguidev_p.png?raw=true" style={{ width: "30px", height: "24px" }} className="d-inline-block align-text-top" />
+					</span>
+
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+
 				</div>
 			</div>
 		</nav>
