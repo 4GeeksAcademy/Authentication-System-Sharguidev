d06efc3378cb04f661a9b3b35f27d0fbdd2e835f
Sharguidev <guillermoj.obando@gmail.com>
2024-11-06 10:35:17 -0600

    Reemplazo de imagen

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -8,7 +8,7 @@ export const Navbar = () => {
 			<div className="container-fluid py-1">
 				<Link to="/">
 					<span className="navbar-brand mb-0">
-						<img alt="imagen" src="https://github.com/4GeeksAcademy/Authentication-System-Sharguidev/blob/main/src/front/img/Sharguidev_p.png?raw=true" style={{ width: "30px", height: "24px" }} className="d-inline-block align-text-top" />
+						<img alt="imagen" src="https://github.com/4GeeksAcademy/Authentication-System-Sharguidev/blob/main/src/front/img/Sharguidev_p.png?raw=true" style={{ width: "100px", height: "96px" }} className="d-inline-block align-text-top" />
 					</span>
 
 				</Link>
